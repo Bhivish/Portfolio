27851d7d67b7fe1bf4415f59197e37e431d7b46c
Bhivish <yeshseessa@gmail.com>
2025-06-10 10:42:24 +0400

    Added html pages for the 3 projects

--- script.js
+++ script.js
@@ -138,6 +138,53 @@ window.swiperCarousel = swiperCarousel;
 
 
 
+function showImageWhenReady() {
+    const image1Images = document.querySelectorAll('.image1Containers .image1');
+    const coverImageImages = document.querySelectorAll('.coverImage');
+    
+    image1Images.forEach((img) => {
+      const container = img.closest('.image1Containers'); 
+      if (!img || !container) return;
+  
+      if (img.complete) {
+        img.classList.add('loaded');
+        img.style.opacity = "1"; 
+      } else {
+        img.addEventListener('load', () => {
+          img.classList.add('loaded');
+          img.style.opacity = "1"; 
+        });
+      }
+    });
+  
+    // Handle coverImage images
+    coverImageImages.forEach((img) => {
+      const container = img.closest('.coverImageContainer'); 
+      
+      if (!img || !container) return;
+  
+      if (img.complete) {
+        img.classList.add('loaded');
+        img.style.opacity = "1"; 
+        container.style.backgroundColor = 'white';
+      } else {
+        img.addEventListener('load', () => {
+          img.classList.add('loaded');
+          img.style.opacity = "1"; 
+        });
+      }
+    });
+  }
+  
+  window.addEventListener("pageshow", showImageWhenReady); 
+  document.addEventListener("DOMContentLoaded", showImageWhenReady);
+  window.showImageWhenReady = showImageWhenReady;
+
+
+
+
+
+
 
 function smoothScroll() {
   const lenis = new Lenis();
@@ -274,6 +321,7 @@ $(function () {
           if (window.initCustomCursor) initCustomCursor();
           if (window.initNavbar) initNavbar();
           if (window.initSidebar) initSidebar();
+          if (window.showImageWhenReady) showImageWhenReady();
           if (window.smoothScroll) smoothScroll();
           if (window.swiperCarousel) swiperCarousel();
 
@@ -303,6 +351,8 @@ $(function () {
   if (window.initSidebar) initSidebar();
   if (window.smoothScroll) smoothScroll();
   if (window.swiperCarousel) swiperCarousel();
+  if (window.showImageWhenReady) showImageWhenReady();
+
 
 });
 
