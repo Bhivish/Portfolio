4a9e52b7a575823abbbc41060059b5e0cabdfd00
Bhivish <yeshseessa@gmail.com>
2025-06-15 17:40:07 +0400

    Changed Image Slider

--- script.js
+++ script.js
@@ -87,42 +87,42 @@ window.initNavbar = initNavbar;
 
 
 
-function swiperCarousel() {
-  const swiper = new Swiper('.myGallerySwiper', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+// function swiperCarousel() {
+//   const swiper = new Swiper('.myGallerySwiper', {
+//     effect: 'coverflow',
+//     grabCursor: true,
+//     centeredSlides: true,
+//     loop: true,
+//     coverflowEffect: {
+//       rotate: 0,
+//       stretch: 0,
+//       depth: 100,
+//       modifier: 1,
+//       slideShadows: true,
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     autoplay: {
+//       delay: 3000,
+//       disableOnInteraction: false,
+//     },
 
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-      },
-      450: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 4,
-      }
-    }
-  });
-}
-window.swiperCarousel = swiperCarousel;
+//     breakpoints: {
+//       320: {
+//         slidesPerView: 1,
+//       },
+//       450: {
+//         slidesPerView: 2,
+//       },
+//       1024: {
+//         slidesPerView: 4,
+//       }
+//     }
+//   });
+// }
+// window.swiperCarousel = swiperCarousel;
 
 
 
@@ -179,6 +179,91 @@ function smoothScroll() {
   requestAnimationFrame(raf);
 }
 window.smoothScroll = smoothScroll; 
+
+
+
+
+
+
+function initImages() {
+
+  const track = document.getElementById("image-track");
+  const wrapper = document.querySelector(".image-track-wrapper");
+
+  if (!track || !wrapper) return;
+
+  let mouseDownAt = 0;
+  let prevPercentage = 0;
+
+  const handleOnDown = (clientX) => {
+    mouseDownAt = clientX;
+    prevPercentage = parseFloat(track.dataset.percentage) || 0;
+  };
+
+  const handleOnUp = () => {
+    mouseDownAt = 0;
+    track.dataset.percentage = track.dataset.percentage || "0";
+  };
+
+  const handleOnMove = (clientX) => {
+    if (mouseDownAt === 0) return;
+
+    const mouseDelta = mouseDownAt - clientX;
+    const maxDelta = window.innerWidth / 2;
+
+    let percentage = (mouseDelta / maxDelta) * -15;
+    let nextPercentageUnconstrained = prevPercentage + percentage;
+
+    const trackRect = track.getBoundingClientRect();
+    const wrapperRect = wrapper.getBoundingClientRect();
+
+    const overflowPx = trackRect.width - wrapperRect.width;
+    const maxNegative = -(overflowPx / trackRect.width) * 100;
+
+    const nextPercentage = Math.min(0, Math.max(nextPercentageUnconstrained, maxNegative));
+    track.dataset.percentage = nextPercentage;
+
+    track.style.transform = `translate(${nextPercentage}%, 0%)`;
+
+    for (const image of track.getElementsByClassName("image")) {
+      image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+  };
+
+  wrapper.addEventListener('mousedown', e => {
+    e.preventDefault();
+    handleOnDown(e.clientX);
+  });
+
+  window.addEventListener('mouseup', () => handleOnUp());
+  window.addEventListener('mousemove', e => handleOnMove(e.clientX));
+
+  wrapper.addEventListener('touchstart', e => {
+    if (e.touches.length > 0) {
+      handleOnDown(e.touches[0].clientX);
+    }
+  }, { passive: true });
+
+  window.addEventListener('touchend', () => handleOnUp());
+  window.addEventListener('touchmove', e => {
+    if (e.touches.length > 0) {
+      handleOnMove(e.touches[0].clientX);
+    }
+  }, { passive: true });
+
+}
+window.initImages = initImages;
+
+
+
+
+
+function lucideIcons() {
+  lucide.createIcons();
+}
+window.init = lucideIcons;
+
+
 
 
 
@@ -288,7 +373,8 @@ $(function () {
           if (window.showImageWhenReady) showImageWhenReady();
           if (window.smoothScroll) smoothScroll();
           if (window.initDropDown) initDropDown();
-          if (window.swiperCarousel) swiperCarousel();
+          if (window.initImages) initImages();
+          if (window.lucideIcons) lucideIcons();
           await startAnimations(); 
         }
       }
@@ -301,8 +387,9 @@ $(function () {
   if (window.initNavbar) initNavbar();
   if (window.initSidebar) initSidebar();
   if (window.smoothScroll) smoothScroll();
-  if (window.swiperCarousel) swiperCarousel();
   if (window.initDropDown) initDropDown();
+  if (window.initImages) initImages();
+  if (window.lucideIcons) lucideIcons();
   if (window.showImageWhenReady) showImageWhenReady();
 
 
